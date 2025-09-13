afea5fd44c576ef575b4c1b6fac12076fab3cbd8
MauricioScheffer <mauricioscheffersilveira13@gmail.com>
2025-09-13 01:42:03 -0300

    Criando index, pastas de icones e css iniciais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import '../styles/index.css';
+import '../styles/Navbar.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import Head from 'next/head';
-import Navbar from '../components/Navbar';
-
+// import Navbar from '../components/Navbar';
+//import Stylesheet from '../styles/index.css';
 const Home = () => {
     return (
-        <div>
-            <Head>
-                <title>Home - Next.js PWA</title>
-                <meta name="description" content="Welcome to the Next.js PWA application." />
-            </Head>
-            <Navbar />
-            <main>
-                <h1>Welcome to the Next.js PWA Application</h1>
-                <p>This is the homepage. Please log in or register to continue.</p>
-            </main>
-        </div>
+
+        <body>
+            <div className="container">
+                <Head>
+                    <title>Care!</title>
+                    <meta name="description" content="Encontre Um Novo Amigo." />
+                </Head>
+                <nav>
+                    <div className="container">
+                        <img src="" alt="" />
+                        <h1>ConnectCare</h1>
+                        <p>Conectando corações bons</p>
+                    </div>
+                </nav>
+
+
+                <main>
+                    <div className="meio">
+                        <h1>Encontre Um Novo Amigo.</h1>
+                        <p>This is the homepage. Please log in or register to .</p>
+                    </div>
+                </main>
+
+                {/* <footer>
+                <div className="pagination">
+                    <a href=""><img src="" alt="" />Inicio</a>
+                    <a href=""><img src="" alt="" />Atividades</a>
+                    <a href=""><img src="" alt="" />Usuário</a>
+                    
+                </div>
+            </footer> */}
+            </div>
+        </body>
     );
 };
 
